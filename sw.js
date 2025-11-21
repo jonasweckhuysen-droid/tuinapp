814b24c603b1514d33bab642096bff65f53d437f
jonasweckhuysen-droid <jonasweckhuysen@gmail.com>
2025-11-21 16:06:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
+const CACHE_NAME = "tuinapp-cache-v1";
+const ASSETS_TO_CACHE = [
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/images/logo-192.png",
+  "/images/logo-512.png"
+];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("tuinapp-cache").then((cache) => {
-      return cache.addAll(["/", "/index.html"]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
@@ -10,6 +19,18 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
     })
   );
 });
